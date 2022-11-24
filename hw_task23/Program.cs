42a13ecdec72436a78e745517036cb623983101e
AleksanderSamokhvalov <samokhvalov2987@gmail.com>
2022-11-24 17:09:44 +0300

    добавлен код для решения задачи №23

--- hw_task23/Program.cs
+++ hw_task23/Program.cs
@@ -4,3 +4,18 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine ("Программа для расчета кубов ряда чисел от 1 до N. V 1.0");
+
+Console.WriteLine ("Введите целое число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count =1;
+
+while (count<=n)
+{
+    Console.WriteLine(count*count*count);
+    // Console.Write( );
+    count++;
+}
